fee9749147c696fbb42548feb2cf08228da21085
BISWAJIT  PATNAIK <112742314+Biswajit1693@users.noreply.github.com>
2022-10-15 20:08:53 +0530

    Add files via upload

---
+++ methodandopstr.py
@@ -0,0 +1,26 @@
+a="BiswajitPatnaik"
+
+print(a.upper())#uppercase
+print(a.lower())#lowercase
+print(a.title())#convertstring into titlecase
+#a[0]="B"#we cannot assign data type to individual letter of inputed string
+print(a.swapcase())#converts all uppercase to lowercase and vice-versa
+print(a.strip("B"))#it removes both leading and trailing characters
+print(a.startswith("B",0,1))#checks the starting character and returns boolean
+print(a.rstrip("aik"))#removes trailing characters
+print(a.rpartition("P"))#PARTITION THE STRING BASED ON THE INPUT
+print(a.rindex("P"))#find the substring inside the string
+print(a.rindex("sw",0,4))#returns the highest index of the substring inside the string if the substring is found. Otherwise, it raises ValueError
+print(a.split("i"))#splits strings based on input charcter,the input character is removed
+#formating string literals
+print("{}".format("age 29"))
+print('This is {1} {0}'.format('me','mine'))
+#formating floating no
+result=45.86376
+print('value of x is {}'.format(result))
+print("value of x precise is {x:1.3f}".format(x=result))#to format float we need to assign another variable to the existing variable inside print
+#fstring format
+print(f"My name is {a}")
+print(f"My name is {a!r}")#here "!r" helps to quote the string in the printout stmnt
+print(f'value of x is {result:1.3f}')#compare line 21 to line 25
+
